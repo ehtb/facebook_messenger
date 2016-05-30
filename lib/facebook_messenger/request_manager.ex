5a946f0911106ccb98963a985cbbd935a6da3bdf
Edo <i@edo.me>
2016-05-30 17:49:02 +0200

    Rename request manager

--- lib/facebook_messenger/request_manager.ex
+++ lib/facebook_messenger/request_manager.ex
@@ -1,18 +1,16 @@
 defmodule FacebookMessenger.RequestManager do
   @moduledoc """
-  module respinsible to post a request to facebook
+  module responsible to post a request to facebook
   """
   def post(url: url, body: body) do
-    HTTPotion.post url,
-    body: body, headers: ["Content-Type": "application/json"]
+    HTTPotion.post(url, body: body, headers: ["Content-Type": "application/json"])
   end
 end
 
 defmodule FacebookMessenger.RequestManager.Mock do
   @moduledoc """
-  moc respinsible to post a request to facebook
+  mock responsible to post a request to facebook
   """
-
   def post(url: url, body: body) do
     send(self, %{url: url, body: body})
   end
